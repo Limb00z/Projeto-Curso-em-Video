80f4565a5f0c633e0588c515cb117a2e6a4b0243
Ruan Reis <86503203+Limb00z@users.noreply.github.com>
2023-01-22 14:44:08 -0300

    criação da função trocaFoto e estilização

--- SiteHoras/script.js
+++ SiteHoras/script.js
@@ -7,4 +7,33 @@ var minutos = new Date().getMinutes();
 var horaPagina = document.getElementById("hora");
 
 //atribuindo escrita de horario atual na página
-horaPagina.innerHTML = `<p>Horário atual: ${hora}:${minutos} </p>`;
+horaPagina.innerHTML = `<p class="txtHora">Horário atual: <strong>${hora}:${minutos}</strong> </p>`;
+
+//pegando a div da imagem pelo id
+var imgPagina = document.getElementById("imagem");
+
+//adcionando imagem
+imgPagina.innerHTML = ` <img src="./img/tarde.jpg" class="imgAdd" alt="sol com folhas">`;
+
+function trocaFoto() {
+    switch (hora) {
+        case 0:
+            imgPagina.innerHTML = ` <img src="./img/madrugada.jpg" class="imgAdd" alt="Noite de Lua">`;
+            break;
+        case 5:
+            imgPagina.innerHTML = ` <img src="./img/manha.jpg" class="imgAdd" alt="sol amanhecendo">`;
+            break;
+        case 12:
+            imgPagina.innerHTML = ` <img src="./img/tarde.jpg" class="imgAdd" alt="sol amanhecendo">`;
+            break;
+        case 16:
+            imgPagina.innerHTML = ` <img src="./img/fimDeTarde.jpg" class="imgAdd" alt="sol com folhas">`;
+            break;
+        case 18:
+            imgPagina.innerHTML = ` <img src="./img/noite.jpg" class="imgAdd" alt="sol com folhas">`;
+            break;
+        default:
+            break;
+                
+    }
+}
