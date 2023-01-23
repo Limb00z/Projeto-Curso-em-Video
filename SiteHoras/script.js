ff5ea10fee258a49913f263db8283c55a6cdd3db
Ruan Reis <86503203+Limb00z@users.noreply.github.com>
2023-01-23 03:20:31 -0300

    Finalizado o projeto, será feito responsividade

--- SiteHoras/script.js
+++ SiteHoras/script.js
@@ -1,5 +1,6 @@
 //pegando a hora local
 var hora = new Date().getHours();
+
 //pegando minuto local
 var minutos = new Date().getMinutes();
 
@@ -19,75 +20,102 @@ function trocaFoto() {
     switch (hora) {
         case 0:
             imgPagina.innerHTML = ` <img src="./img/madrugada.jpg" class="imgAdd" alt="mão saindo do tumulo">`;
+            document.body.style.background = '#0a1d70';
             break;
         case 1:
             imgPagina.innerHTML = ` <img src="./img/madrugada.jpg" class="imgAdd" alt="mão saindo do tumulo">`;
+            document.body.style.background = '#0a1d70';
             break;
         case 2:
             imgPagina.innerHTML = ` <img src="./img/madrugada.jpg" class="imgAdd" alt="mão saindo do tumulo">`;
+            document.body.style.background = '#0a1d70';
             break;
         case 3:
             imgPagina.innerHTML = ` <img src="./img/madrugada.jpg" class="imgAdd" alt="mão saindo do tumulo">`;
+            document.body.style.background = '#0a1d70';
             break;
         case 4:
             imgPagina.innerHTML = ` <img src="./img/madrugada.jpg" class="imgAdd" alt="mão saindo do tumulo">`;
+            document.body.style.background = '#0a1d70';
+            
             break;
         case 5:
             imgPagina.innerHTML = ` <img src="./img/manha.jpg" class="imgAdd" alt="homem no campo agradecendo pelo sol">`;
+            document.body.style.background = '#38a4b7ab';
             break;
         case 6:
             imgPagina.innerHTML = ` <img src="./img/manha.jpg" class="imgAdd" alt="homem no campo agradecendo pelo sol">`;
+            document.body.style.background = '#38a4b7ab';
             break;
         case 7:
             imgPagina.innerHTML = ` <img src="./img/manha.jpg" class="imgAdd" alt="homem no campo agradecendo pelo sol">`;
+            document.body.style.background = '#38a4b7ab';
             break;
         case 8:
             imgPagina.innerHTML = ` <img src="./img/manha.jpg" class="imgAdd" alt="homem no campo agradecendo pelo sol">`;
+            document.body.style.background = '#38a4b7ab';
             break;
         case 9:
             imgPagina.innerHTML = ` <img src="./img/manha.jpg" class="imgAdd" alt="homem no campo agradecendo pelo sol">`;
+            document.body.style.background = '#38a4b7ab';
             break;
         case 10:
             imgPagina.innerHTML = ` <img src="./img/manha.jpg" class="imgAdd" alt="homem no campo agradecendo pelo sol">`;
+            document.body.style.background = '#38a4b7ab';
             break;
         case 11:
             imgPagina.innerHTML = ` <img src="./img/manha.jpg" class="imgAdd" alt="homem no campo agradecendo pelo sol">`;
+            document.body.style.background = '#38a4b7ab';
             break;
         case 12:
             imgPagina.innerHTML = ` <img src="./img/tarde.jpg" class="imgAdd" alt="sol forte em um coqueiro">`;
+            document.body.style.background = '#f09d18ce';
             break;
         case 13:
             imgPagina.innerHTML = ` <img src="./img/tarde.jpg" class="imgAdd" alt="sol forte em um coqueiro">`;
+            document.body.style.background = '#f09d18ce';
             break;
+
         case 14:
             imgPagina.innerHTML = ` <img src="./img/tarde.jpg" class="imgAdd" alt="sol forte em um coqueiro">`;
+            document.body.style.background = '#f09d18ce';
             break;
         case 15:
             imgPagina.innerHTML = ` <img src="./img/tarde.jpg" class="imgAdd" alt="sol forte em um coqueiro">`;
+            document.body.style.background = '#f09d18ce';
             break;
         case 16:
             imgPagina.innerHTML = ` <img src="./img/fimDeTarde.jpg" class="imgAdd" alt="sol se pondo na praia">`;
+            document.body.style.background = '#f09d18ce';
             break;
         case 17:
             imgPagina.innerHTML = ` <img src="./img/fimDeTarde.jpg" class="imgAdd" alt="sol se pondo na praia">`;
+            document.body.style.background = '#f09d18ce';
             break;
         case 18:
-            imgPagina.innerHTML = ` <img src="./img/noite.jpg" class="imgAdd" alt="lua cheia e arvores">`;
+            imgPagina.innerHTML = ` <img src="./img/fimDeTarde.jpg" class="imgAdd" alt="lua cheia e arvores">`;
+            
+            document.body.style.background = '#f09d18ce';
             break;
         case 19:
             imgPagina.innerHTML = ` <img src="./img/noite.jpg" class="imgAdd" alt="lua cheia e arvores">`;
+            document.body.style.background = '#3b4986';
             break;
         case 20:
             imgPagina.innerHTML = ` <img src="./img/noite.jpg" class="imgAdd" alt="lua cheia e arvores">`;
+            document.body.style.background = '#3b4986';
             break;
         case 21:
             imgPagina.innerHTML = ` <img src="./img/noite.jpg" class="imgAdd" alt="lua cheia e arvores">`;
+            document.body.style.background = '#3b4986';
             break;
         case 22:
             imgPagina.innerHTML = ` <img src="./img/noite.jpg" class="imgAdd" alt="lua cheia e arvores">`;
+            document.body.style.background = '#3b4986';
             break;
         case 23:
             imgPagina.innerHTML = ` <img src="./img/noite.jpg" class="imgAdd" alt="lua cheia e arvores">`;
+            document.body.style.background = '#3b4986';
             break;
         default:
             break;
