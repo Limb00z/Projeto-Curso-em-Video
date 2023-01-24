dd424fd189d7e3ddadf876a528fb35f0a5208dac
Ruan Reis <86503203+Limb00z@users.noreply.github.com>
2023-01-23 23:45:53 -0300

    add img e estrutura de seleção

---
+++ Idade&Sexo/script.js
@@ -0,0 +1,73 @@
+function verificar() {
+    var dataAtual = new Date().getFullYear();
+    console.log(dataAtual);
+    
+    var anoNascimento = Number(document.getElementById("txtano").value);
+    console.log(anoNascimento);
+
+    var res = document.getElementById("res"); //div com a resposta
+
+    if (anoNascimento <= 1900 || anoNascimento > dataAtual) {
+        alert("[ERRO]Verifique os dados e tente novamente.");
+    
+    } else {
+        var idade = dataAtual - anoNascimento; //calculando idade atual
+        console.log(`idade atual: ${idade}`);
+
+        var img = document.createElement("img"); //criando um elemento(tag) no html, como se fosse diretamente
+        img.setAttribute('id', 'foto'); //atribuindo um id para podermos manipular ele dps.
+
+        
+        var fsex = document.getElementsByName('radsex'); //pegando a posição no array 
+        var genero = "";
+        if (fsex[0].checked) {                              //e atribuindo sexo e idade p/ foto
+            genero = 'Homem'
+            if (idade >= 0 && idade <= 3) {
+                //bb
+                img.setAttribute('src', './img/bb-m.jpg');
+            } else if (idade > 3 && idade < 10  ) {
+                //criança
+                img.setAttribute('src', './img/crianca-m.jpg');
+            } else if (idade >= 10  && idade < 15) {
+                //adolescente
+                img.setAttribute('src', './img/adolescente-m.jpg');
+            } else if (idade >= 15 ) {
+                //jovem
+                img.setAttribute('src', './img/jovem-m.jpg');
+            } else if (idade >= 30 && idade < 50 ){
+                //adulto
+                img.setAttribute('src', './img/adulto-m.jpg');
+            } else {
+                //idoso
+                img.setAttribute('src', './img/idoso-m.jpg');
+            };
+
+
+        } else {
+            genero = 'Mulher'
+            if (idade >= 0 && idade <= 3) {
+                //bb
+                img.setAttribute('src', './img/bb-f.jpg');
+            } else if (idade > 3 && idade < 10  ) {
+                //criança
+                img.setAttribute('src', './img/crianca-f.jpg');
+            } else if (idade >= 10  && idade < 15) {
+                //adolescente
+                img.setAttribute('src', './img/adolescente-f.jpg');
+            } else if (idade >= 15 ) {
+                //jovem
+                img.setAttribute('src', './img/jovem-f.jpg');
+            } else if (idade >= 30 && idade < 50 ){
+                //adulto
+                img.setAttribute('src', './img/adulto-f.jpg');
+            } else {
+                //idoso
+                img.setAttribute('src', './img/idoso-f.jpg');
+            };
+        };
+        console.log(genero);                             //testando genero
+        
+    }
+    
+}
+
