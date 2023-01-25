a74ebc743a919d02e15aef17e1d871d0e4a236e7
Ruan Reis <86503203+Limb00z@users.noreply.github.com>
2023-01-25 02:36:11 -0300

    comentários

--- Calculadora/script.js
+++ Calculadora/script.js
@@ -1,10 +1,10 @@
 function calcular() {
-    var numero = Number(document.getElementById('numero').value); //se pegar sem a função Number(), vem com string.
+    var numero = Number(document.getElementById('numero').value); //se pegar sem a função Number(), vem com string. ---não esqueça do value
     console.log(typeof numero, numero); //teste de tipo e valor.
 
     var multi = document.getElementById("multiplicados"); // Pegando o objeto textarea para atribuir
 
-    // var c = 0;
+    // var c = 0; //não recisa mais, pois criei dentro do for, explicar isso dps
 
     if (numero < 0) {
         alert("[ERRO] Numero inválido: MINÍMO É 0 ");
