422890742de3f9866897d9c3dcb1b51fbddccf12
Ruan Reis <86503203+Limb00z@users.noreply.github.com>
2023-01-25 02:14:37 -0300

    estrutura, script e estilização OK. Falta <BR>

---
+++ Calculadora/script.js
@@ -0,0 +1,16 @@
+function calcular() {
+    var numero = Number(document.getElementById('numero').value); //se pegar sem a função Number(), vem com string.
+    console.log(typeof numero, numero); //teste de tipo e valor.
+
+    var multi = document.getElementById("multiplicados"); // Pegando o objeto textarea para atribuir
+
+    // var c = 0;
+
+    if (numero < 0) {
+        alert("[ERRO] Numero inválido: MINÍMO É 0 ");
+    } else {
+        for (var c = 0; c <= 10; c++){
+            multi.innerHTML += `${numero} x ${c} = ${numero * c} `;            
+        }
+    }
+}
