a357ed5a625de73945a107a2298a2dc3777cdb09
Ruan Reis <86503203+Limb00z@users.noreply.github.com>
2023-01-24 22:15:58 -0300

    finalizado o script

--- Repetições/script.js
+++ Repetições/script.js
@@ -6,19 +6,40 @@ function contar() {
     var fim = Number(document.getElementById("fim").value);
     var passo = Number(document.getElementById("passo").value);
 
-    console.log(inicio,fim,passo)
+    console.log(inicio, fim, passo)
     var res = document.getElementById('res');
 
-    if (passo == 0) {
-        alert("Você consegue andar parado? Passo não pode ser 0, será reatribuido para 1 :} ")
-    } else {
-        do {
-           res.innerHTML = `${inicio} -  `;
-            inicio = inicio + passo; 
-        } while (inicio < fim) 
+    if (passo <= 0 || fim == 0 || inicio == 0) {
+        alert("Você consegue andar parado? não pode ser 0, será reatribuido para 1 :} ");
+        res.innerHTML = 'Impossível contar...'
+        passo = 1
+    } else { //lembra de testar
+        // res.innerHTML = 'Contando';
+
+        // for (var c = inicio; c <= fim; c += passo){ //fazer esse primeiro para exemplo
+        //     res.innerHTML += ` ${c}`  // Aqui ele atualiza o que já tem e concatena.
+        // }
+        // res.innerHTML += `\u{1F3C1}` // bandeirinha
+
+
+        if (inicio < fim) { // esse é o segundo exemplo para correção do inicio ser menor que o fim.
+            for (var c = inicio; c <= fim; c += passo){ //fazer esse primeiro para exemplo
+                res.innerHTML += ` ${c}`  // Aqui ele atualiza o que já tem e concatena.
+            }
+            //res.innerHTML += `\u{1F3C1}` // bandeirinha  -> ela seria duplicada dentro do if e else, para isso, basta colocar do lado de fora 
+        } else { // contagem decrescente
+            for (var c = inicio; c >= fim; c -= passo){
+                res.innerHTML += ` ${c}`  // Aqui ele atualiza o que já tem e concatena.
+            }
+        }
+        res.innerHTML += `\u{1F3C1}` // bandeirinha
+
+
+
+        // while (inicio < fim) {
+        //     
+        //     inicio = passo + inicio;
             
-        
+        // }
     }
- 
-    
-}
+} 
